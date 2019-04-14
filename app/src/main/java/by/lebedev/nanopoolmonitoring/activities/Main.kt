942f75dec8742e6176c095dffa31f3ca01fa0bd7
Olga Kurcheuskaya <O.Kurcheuskaya@tut.by>
2019-04-14 22:13:47 +0300

    5  fragment

--- app/src/main/java/by/lebedev/nanopoolmonitoring/activities/Main.kt
+++ app/src/main/java/by/lebedev/nanopoolmonitoring/activities/Main.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import by.lebedev.nanopoolmonitoring.R
+import kotlinx.android.synthetic.main.main_layout.*
 
 class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.main_layout)
 
+
+        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+        viewpager_main.adapter = fragmentAdapter
+
+
+        tabs_main.setupWithViewPager(viewpager_main)
     }
-}
+
+    }
---
+++ app/src/main/java/by/lebedev/nanopoolmonitoring/activities/MyPageAdapter.kt
@@ -0,0 +1,36 @@
+package by.lebedev.nanopoolmonitoring.activities
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            2 -> ThirdFragment()
+            3 -> FourFragment()
+            else -> {
+                return FiveFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 5
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "First Tab"
+            1 -> "Second Tab"
+            2 -> "Third Tab"
+            3 -> "Four  Tab"
+            else -> {
+                return "Five  Tab"
+            }
+        }
+    }
+}
