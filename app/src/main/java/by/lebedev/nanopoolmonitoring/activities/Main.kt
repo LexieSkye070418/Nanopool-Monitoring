5363108c9677d70ea88143645e94364b1bab66a5
Olga Kurcheuskaya <O.Kurcheuskaya@tut.by>
2019-04-14 23:14:34 +0300

    5 табов

--- app/src/main/java/by/lebedev/nanopoolmonitoring/activities/Main.kt
+++ app/src/main/java/by/lebedev/nanopoolmonitoring/activities/Main.kt
@@ -1,18 +1,20 @@
 package by.lebedev.nanopoolmonitoring.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import by.lebedev.nanopoolmonitoring.R
 import kotlinx.android.synthetic.main.main_layout.*
 
+
 class Main : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
 
 
         val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+
         viewpager_main.adapter = fragmentAdapter
 
 
--- app/src/main/java/by/lebedev/nanopoolmonitoring/activities/MyPageAdapter.kt
+++ app/src/main/java/by/lebedev/nanopoolmonitoring/activities/MyPageAdapter.kt
@@ -8,6 +8,7 @@ class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
+
             0 -> FirstFragment()
             1 -> SecondFragment()
             2 -> ThirdFragment()
