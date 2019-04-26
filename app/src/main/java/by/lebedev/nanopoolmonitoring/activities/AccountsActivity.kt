caa19c2f1568a946eee3c3354860e4e9c1b4873a
Olga Kurcheuskaya <O.Kurcheuskaya@tut.by>
2019-04-26 19:09:03 +0300

    addReclama

--- app/src/main/java/by/lebedev/nanopoolmonitoring/activities/AccountsActivity.kt
+++ app/src/main/java/by/lebedev/nanopoolmonitoring/activities/AccountsActivity.kt
@@ -6,24 +6,44 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.widget.Toast
+import by.lebedev.nanopoolmonitoring.R
 import by.lebedev.nanopoolmonitoring.activities.recycler.accounts.AccountAdapter
 import by.lebedev.nanopoolmonitoring.dagger.AccountLocalList
 import by.lebedev.nanopoolmonitoring.dagger.provider.DaggerMagicBox
 import by.lebedev.nanopoolmonitoring.room.DataBase
 import by.lebedev.nanopoolmonitoring.room.entity.Account
+import com.google.android.gms.ads.AdRequest
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.accounts_layout.*
 import javax.inject.Inject
+import com.google.android.gms.ads.AdView
+
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.MobileAds
+
 
 class AccountsActivity : AppCompatActivity() {
     private var back_pressed: Long = 0
+
+    lateinit var mAdView : AdView
+
     @Inject
     lateinit var accountLocalList: AccountLocalList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(by.lebedev.nanopoolmonitoring.R.layout.accounts_layout)
+        setContentView(R.layout.accounts_layout)
+
+
+        MobileAds.initialize(this,"ca-app-pub-9699134137611847~3977929109")
+        val adView = AdView(this)
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = "ca-app-pub-9699134137611847/6712027369"
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         val component = DaggerMagicBox.builder().build()
         accountLocalList = component.provideAccountLocalList()
